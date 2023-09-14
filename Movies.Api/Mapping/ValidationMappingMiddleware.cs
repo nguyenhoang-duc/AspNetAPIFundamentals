32d5ede10373780532c41ab5edef60f9a3b85a30
Hoang Duc Nguyen <hoang-duc.nguyen@lhsystems.com>
2023-09-14 22:59:27 +0200

    Implementing Validation: Added comments for explanation

--- Movies.Api/Mapping/ValidationMappingMiddleware.cs
+++ Movies.Api/Mapping/ValidationMappingMiddleware.cs
@@ -7,15 +7,21 @@ namespace Movies.Api.Mapping
     {
         private readonly RequestDelegate next; 
 
+        // The request delegate is method of the next middleware 
+        // because this middleware is right before the controller, the next will be actually the controller call
         public ValidationMappingMiddleware(RequestDelegate next) 
         {
             this.next = next; 
         }
 
+        // Each middleware in an API has to have the Invoke or InvokeAsync Method implemented
         public async Task InvokeAsync(HttpContext context)
         {
             try
             {
+                // the next delegate is actually the controller 
+                // so let the controller handle the request 
+                // if the request fails because of validation, catch it and handle it
                 await next(context); 
             }
             catch (ValidationException ex)
