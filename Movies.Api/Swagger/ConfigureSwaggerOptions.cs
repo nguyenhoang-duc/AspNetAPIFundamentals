1d4657e334e09e7eddd66b9dcffc359cc7c36e6d
Hoang Duc Nguyen <hoang.d1997@gmail.com>
2024-04-30 18:34:14 +0200

    Added authentication to swagger

--- Movies.Api/Swagger/ConfigureSwaggerOptions.cs
+++ Movies.Api/Swagger/ConfigureSwaggerOptions.cs
@@ -27,6 +27,31 @@ namespace Movies.Api.Swagger
                     Version = description.ApiVersion.ToString(),
                 });
             }
+
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header, 
+                Description = "Please provide a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
         }
     }
 }
