f25db85d9c80e8b0722fc1bd7a73deb730b6f68c
Hoang Duc Nguyen <hoang.d1997@gmail.com>
2024-05-06 22:12:57 +0200

    Added static authentication using a refit client

--- Movies.Api.Consumer/Program.cs
+++ Movies.Api.Consumer/Program.cs
@@ -1,9 +1,33 @@
-﻿using Movies.Api.Sdk;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Movies.Api.Sdk;
+using Movies.Contracts.Requests;
 using Refit;
 using System.Text.Json;
 
-var moviesApi = RestService.For<IMoviesApi>("https://localhost:5001");
+var services = new ServiceCollection();
 
-var movie = await moviesApi.GetMoviesAsync("test-3-2023");
+services.AddRefitClient<IMoviesApi>(x => new RefitSettings
+{
+    AuthorizationHeaderValueGetter = (_, _) => Task.FromResult("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyYmM4MzdmYy00MzAxLTQxYTItYWJjMi1hYzZjYzYyZmI3YWEiLCJzdWIiOiJob2FuZy5kMTk5N0BnbWFpbC5jb20iLCJlbWFpbCI6ImhvYW5nLmQxOTk3QGdtYWlsLmNvbSIsInVzZXJpZCI6ImQ4NTY2ZGUzLWIxYTYtNGE5Yi1iODQyLThlMzg4N2E4MmU0MSIsImFkbWluIjp0cnVlLCJ0cnVzdGVkX21lbWJlciI6dHJ1ZSwibmJmIjoxNzE1MDI2MjYyLCJleHAiOjE3MTUwNTUwNjIsImlhdCI6MTcxNTAyNjI2MiwiaXNzIjoiaHR0cHM6Ly9pZC5uaWNrY2hhcHNhcy5jb20iLCJhdWQiOiJodHRwczovL21vdmllcy5uaWNrY2hhcHNhcy5jb20ifQ.lwq7C9QeAxqrljbYtZ7D08jDDlkFjQ2xvEjRAr_38AY")
+})
+.ConfigureHttpClient(x => x.BaseAddress = new Uri("https://localhost:5001")); 
 
-Console.WriteLine(JsonSerializer.Serialize(movie));
+var provider = services.BuildServiceProvider();
+
+var moviesApi = provider.GetRequiredService<IMoviesApi>();
+
+var request = new GetAllMoviesRequest
+{
+    Title = null,
+    Year = null,
+    SortBy = null,
+    Page = 1,
+    PageSize = 5,
+};
+
+var movies = await moviesApi.GetMoviesAsync(request);
+
+foreach (var movie in movies.Items)
+{ 
+    Console.WriteLine(JsonSerializer.Serialize(movie));
+}
--- Movies.Api.Sdk/IMoviesApi.cs
+++ Movies.Api.Sdk/IMoviesApi.cs
@@ -1,11 +1,16 @@
-﻿using Movies.Contracts.Responses;
+﻿using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
 using Refit;
 
 namespace Movies.Api.Sdk
 {
+    [Headers("Authorization: Bearer")]
     public interface IMoviesApi
     {
         [Get(ApiEndpoints.Movies.Get)]
         Task<MovieResponse> GetMoviesAsync(string idOrSlug);
+
+        [Get(ApiEndpoints.Movies.GetAll)]
+        Task<MoviesResponse> GetMoviesAsync(GetAllMoviesRequest request); 
     }
 }
