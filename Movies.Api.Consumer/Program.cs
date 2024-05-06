7c3bdac3d509cb8a3c20c00d991f021c0e8f9268
Hoang Duc Nguyen <hoang.d1997@gmail.com>
2024-05-06 21:56:52 +0200

    Added simple sdk using refit for the get endpoint

---
+++ Movies.Api.Consumer/Program.cs
@@ -0,0 +1,9 @@
+﻿using Movies.Api.Sdk;
+using Refit;
+using System.Text.Json;
+
+var moviesApi = RestService.For<IMoviesApi>("https://localhost:5001");
+
+var movie = await moviesApi.GetMoviesAsync("test-3-2023");
+
+Console.WriteLine(JsonSerializer.Serialize(movie));
